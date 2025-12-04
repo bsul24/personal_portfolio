0a28451a05bf80606d87fae6151d9e42a70f2830
Bill Sullivan <w.o.sullivan24@gmail.com>
2025-12-03 22:26:11 -0700

    Update config file and html filenames

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import {defineConfig} from "vite"
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        cosmoMotors: resolve(__dirname, "cosmoMotors.html"),
+        pawMailMerge: resolve(__dirname, "pawMailMerge.html"),
+        fitnessRoutineGenerator: resolve(
+          __dirname,
+          "fitnessRoutineGenerator.html"
+        ),
+      },
+    },
+  },
+});
